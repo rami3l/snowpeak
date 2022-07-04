0a93b9c88e3d1079420e05104e5b1e9efe155b09
rami3l <rami3l@outlook.com>
2022-07-04 14:22:16 +0200

    feat: (wip) add SNMP Message declarations, pt. 6

--- src/snowpeak-compat.ads
+++ src/snowpeak-compat.ads
@@ -1,5 +1,6 @@
 -- SRC: https://github.com/Componolit/RecordFlux/blob/5dd82231eec29a98a25fe67153a3bd03ccd5d3d0/examples/apps/dhcp_client/src/channel.ads
 
+with Ada.Streams; use Ada.Streams;
 with RFLX.RFLX_Builtin_Types;
 
 package Snowpeak.Compat with
@@ -9,14 +10,14 @@ is
 
    function To_Ada_Stream
      (Buffer : RFLX.RFLX_Builtin_Types.Bytes)
-      return Ada.Streams.Stream_Element_Array with
+      return Stream_Element_Array with
       Pre => Buffer'First = 1
-      and then Buffer'Length <= Ada.Streams.Stream_Element_Offset'Last;
+      and then Buffer'Length <= Stream_Element_Offset'Last;
 
    function To_RFLX_Bytes
-     (Buffer : Ada.Streams.Stream_Element_Array)
+     (Buffer : Stream_Element_Array)
       return RFLX.RFLX_Builtin_Types.Bytes with
       Pre => Buffer'First = 1
       and then Buffer'Length <=
-        Ada.Streams.Stream_Element_Offset (RFLX.RFLX_Builtin_Types.Index'Last);
+        Stream_Element_Offset (RFLX.RFLX_Builtin_Types.Index'Last);
 end Snowpeak.Compat;
