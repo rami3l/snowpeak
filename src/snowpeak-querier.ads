e8be2e36285d03ff06e591263eec7d4e0670599f
rami3l <rami3l@outlook.com>
2022-07-27 15:36:54 +0200

    feat(querier): add support for `GetNextRequest-PDU`

--- src/snowpeak-querier.ads
+++ src/snowpeak-querier.ads
@@ -3,24 +3,31 @@ with Snowpeak.Message;
 with RFLX.RFLX_Types;
 
 package Snowpeak.Querier is
-   package TLV_Options is new Options (Snowpeak.Message.TLV);
+   package Varbind_Options is new Options (Snowpeak.Message.Varbind);
 
    type Querier is interface;
 
    function Get_TLV
      (Self : Querier; OID : RFLX.RFLX_Types.Bytes)
-      return TLV_Options.Option is abstract;
+      return Varbind_Options.Option is abstract;
+
+   function Get_Next_TLV
+     (Self : Querier; OID : RFLX.RFLX_Types.Bytes)
+      return Varbind_Options.Option is abstract;
 
    function Respond
      (Self : Querier'Class; Request : Snowpeak.Message.Message)
       return Snowpeak.Message.Message;
 
    type Map_Querier is new Querier with record
-      Data : Snowpeak.Message.Varbinds.Stack;
+      Data : Snowpeak.Message.Varbinds.Stack; -- An ORDERED list of Varbinds.
    end record;
 
    overriding function Get_TLV
      (Self : Map_Querier; OID : RFLX.RFLX_Types.Bytes)
-      return TLV_Options.Option;
+      return Varbind_Options.Option;
 
+   overriding function Get_Next_TLV
+     (Self : Map_Querier; OID : RFLX.RFLX_Types.Bytes)
+      return Varbind_Options.Option;
 end Snowpeak.Querier;
